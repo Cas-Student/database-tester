db1e993eb26151010515aa3dc0c7e38c5e12f53d
CMacino <123585411+Cas-Student@users.noreply.github.com>
2024-07-25 16:25:51 +0000

    1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+var mysql = require('mysql');
 
+var con = mysql.createConnection({
+    host: "ep-soft-sunset-a478dq9k.us-east-1.pg.koyeb.app",
+    user: "koyeb-adm",
+    password: "yQ4Z6bOzjvgP"
+})
+
+con.connect(function(err) {
+    if (err) throw err;
+    document.getElementById("output").innerText = "Connected!";
+})
